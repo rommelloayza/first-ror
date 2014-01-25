4b14578554567fc0ef3a582548b439ca7d3661ff
Rommel Loayza <rommel.lexlara@gmail.com>
2014-01-25 16:33:18 -0430

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Demo::Application.routes.draw do
+  resources :books
+
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
